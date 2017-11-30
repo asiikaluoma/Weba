e7bf73f0ff2889f68eaf84f0a37007e3594c153a
Alec Siikaluoma <alec.siikaluoma@helsinki.fi>
2017-11-30 12:03:10 +0200

    Change

--- src/main/java/wad/AliveApplication.java
+++ src/main/java/wad/AliveApplication.java
@@ -11,7 +11,7 @@ public class AliveApplication {
     }
 
     public static String githubUrl() {
-        return "";
+        return "https://github.com/AlecSiikaluoma/Weba";
     }
 
     public static String travisUrl() {
